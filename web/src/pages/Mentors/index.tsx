003e987ea3f192a2104b1a3e16a8785d0a265e94
Rafael Akiyoshi <rafah94@hotmail.com>
2020-08-07 22:35:14 -0400

    1st version of mentors page

---
+++ web/src/pages/Mentors/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './styles.css';
+import MentorCard from '../../Mentor-card';
+
+const Mentors = () => {
+  return (
+    <div id="page-mentors-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-header">
+          <h1>MentorHouse</h1>
+        </div>
+      </header>
+      <div className="mentors-cards">
+        <MentorCard 
+          name="Rafael Akiyoshi"
+          description="Software Engineer"
+          bio="O empenho em analisar a estrutura atual da organização nos obriga à análise dos conhecimentos estratégicos para atingir a excelência. Neste sentido, o fenômeno da Internet assume importantes posições no estabelecimento das regras de conduta normativas."
+          photo="https://avatars2.githubusercontent.com/u/11548256?s=460&u=b7e5abe5d147a04e35ba5cf37908df7031a8f697&v=4" 
+          contact="vem de zap"
+          price="35,00"
+        />
+        <MentorCard 
+          name="Rafael Akiyoshi"
+          description="Software Engineer"
+          bio="O empenho em analisar a estrutura atual da organização nos obriga à análise dos conhecimentos estratégicos para atingir a excelência. Neste sentido, o fenômeno da Internet assume importantes posições no estabelecimento das regras de conduta normativas."
+          photo="https://avatars2.githubusercontent.com/u/11548256?s=460&u=b7e5abe5d147a04e35ba5cf37908df7031a8f697&v=4" 
+          contact="vem de zap"
+          price="35,00"
+        />
+        <MentorCard 
+          name="Rafael Akiyoshi"
+          description="Software Engineer"
+          bio="O empenho em analisar a estrutura atual da organização nos obriga à análise dos conhecimentos estratégicos para atingir a excelência. Neste sentido, o fenômeno da Internet assume importantes posições no estabelecimento das regras de conduta normativas."
+          photo="https://avatars2.githubusercontent.com/u/11548256?s=460&u=b7e5abe5d147a04e35ba5cf37908df7031a8f697&v=4" 
+          contact="vem de zap"
+          price="35,00"
+        />
+        <MentorCard 
+          name="Rafael Akiyoshi"
+          description="Software Engineer"
+          bio="O empenho em analisar a estrutura atual da organização nos obriga à análise dos conhecimentos estratégicos para atingir a excelência. Neste sentido, o fenômeno da Internet assume importantes posições no estabelecimento das regras de conduta normativas."
+          photo="https://avatars2.githubusercontent.com/u/11548256?s=460&u=b7e5abe5d147a04e35ba5cf37908df7031a8f697&v=4" 
+          contact="vem de zap"
+          price="35,00"
+        />
+        <MentorCard 
+          name="Rafael Akiyoshi"
+          description="Software Engineer"
+          bio="O empenho em analisar a estrutura atual da organização nos obriga à análise dos conhecimentos estratégicos para atingir a excelência. Neste sentido, o fenômeno da Internet assume importantes posições no estabelecimento das regras de conduta normativas."
+          photo="https://avatars2.githubusercontent.com/u/11548256?s=460&u=b7e5abe5d147a04e35ba5cf37908df7031a8f697&v=4" 
+          contact="vem de zap"
+          price="35,00"
+        />
+        <MentorCard 
+          name="Rafael Akiyoshi"
+          description="Software Engineer"
+          bio="O empenho em analisar a estrutura atual da organização nos obriga à análise dos conhecimentos estratégicos para atingir a excelência. Neste sentido, o fenômeno da Internet assume importantes posições no estabelecimento das regras de conduta normativas."
+          photo="https://avatars2.githubusercontent.com/u/11548256?s=460&u=b7e5abe5d147a04e35ba5cf37908df7031a8f697&v=4" 
+          contact="vem de zap"
+          price="35,00"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Mentors;
